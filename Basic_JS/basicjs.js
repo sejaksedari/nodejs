ddb89a167959769db8b77439909ea3a5e95afb09
sejaksedari <azaimfuad216@gmail.com>
2023-12-05 21:21:51 +0700

    basicjsfilenote

---
+++ Basic_JS/basicjs.js
@@ -0,0 +1,14 @@
+
+
+/*
+
+https://www.youtube.com/watch?v=oPlEq7fewIg&list=PLFIM0718LjIWXagluzROrA-iBY9eeUt4w&index=9&ab_channel=WebProgrammingUNPAS
+
+[9] Data Type in JS
+- Angka:
+    - angka tanpa desimal
+
+
+
+
+*/
