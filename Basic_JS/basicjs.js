045edef8bd6af06ae1b828bc0e77dc8e484806e2
sejaksedari <azaimfuad216@gmail.com>
2023-12-06 08:51:38 +0700

    v3

--- Basic_JS/basicjs.js
+++ Basic_JS/basicjs.js
@@ -4,11 +4,12 @@
 
 https://www.youtube.com/watch?v=oPlEq7fewIg&list=PLFIM0718LjIWXagluzROrA-iBY9eeUt4w&index=9&ab_channel=WebProgrammingUNPAS
 
-[9] Data Type in JS
-
 Max = 64bit, 2^64
 
-[9] Angka_________________________________________________
+
+<< SHORT NOTES >>
+
+[9] Data Type: Angka __________________________________________
     - angka tanpa desimal: 10, 150, (akurat sampe 15 digit)
     - angka dengan desimal: 3.14, 0.5, 100.00 (pecahan ga terlalu akurat)
         - coba console.log(0.2 + 0.1);
@@ -18,7 +19,7 @@ Max = 64bit, 2^64
     Notes:
     - jgn pernah mengawali angka dengan 0, misal console.log(022) atau console.log(0xFF)
 
-[10] Operator_______________________________________________
+[10, 11] Operator ______________________________________________
     - Binary Op (butuh 2 operand: operand1 operator operand2)
         - Aritmatika: +, -, *, /, % (modulo or sisa bagi)
             Operator Precedence Order: (), *, /, +, -
@@ -33,19 +34,55 @@ Max = 64bit, 2^64
             Notes:
                 10 == "10" will return true, meskipun beda tipe datanya, kecuali 10 === "10" pake strict buat bandingin tipe data juga
         - Logika
+            && and
+            || or
+            ! not
         - String
+            op + auto identifikasi operandnya string or numb
+            10 + "10" = "1010"
+            10 + 10 + "10" = "2010"
+            "10" + 10 + 10 = "101010"
+            Intinya, + otomatis ngidentifikasi kalo ada string di operand, lgsg convert jadi string. makanya di contoh ke-2 itu angka + angka jadi 20 dulu baru 2010 karna + ga identifikasi sbg string di operasi pertama
     - Ternary Op (butuh 3 operand)
         - Kondisional
+            (kondisi) ? benar : salah
+            (x % 2 == 0) ? "genap" : "ganjil"
     - Unary Op (butuh 1 operand)
         - Typeof
+            typeof(10); // number
+            typeof("10"); // string
+            typeof(true); // boolean
 
-[11]
+[12] Data Type: String _________________________________________
+    - Escape Character: \" \'
+        misal ada kasus harus nulis string: ""Gerakan Pungut Sampah" dilaksanakan pada hari Jum'at" -> harus ditulis:
+            ' "Gerakan Pungut Sampah" dilaksanakan pada hari Jum\'at ' atau
+            " \"Gerakan Pungut Sampah\" dilaksanakan pada hari Jum'at "
+        Full List Escape Character:
+            \0      Null
+            \'      '
+            \"      "
+            \\      \
+            \n      new line or baris baru
+            \t      tab
+            \b      backspace
+            \uXXXX  unicode
+    - Concatenation: +
+    - String Comparation: 
+        "Fuad" == "fuad" // return false
+    - .length: ngitung panjang string
+        "Fuad Azaim".length // return 10 (space included)
+    
 
 
 
+[X]
 
 
-*/
+/*
+// Example [10, 11] Operator _________________________
+
+// Operator Aritmatika, Penugasan, Perbandingan
 
 var x;
 var result;
@@ -58,3 +95,37 @@ result = 5;
 console.log(x); // 5
 console.log(result == x); // true
 console.log(10 === "10"); // false
+*/
+
+// Operator Logic
+
+/*
+var x;
+x = 8;
+
+// Cek x bilangan genap dan kurang dari 10
+// x % 2 == 0 // cek x bilangan genap, x habis dibagi 2
+// x < 10 // cek x kurang dari 10
+if ((x % 2 == 0) && (x < 10)) {
+    console.log("x genap yang kurang dari 10");
+} else {
+    console.log("x bukan genap yang kurang dari 10");
+}
+
+// inversion
+console.log(!(x < 10)); // aslinya true, diinverse ! jadi false
+*/
+
+// Operator String
+
+
+/*
+a = "apple";
+b = "pen";
+c = a + " " + b;
+
+console.log(c);
+*/
+
+
+// Example [X] Title  _________________________
